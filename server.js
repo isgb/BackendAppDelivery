3d8ef97f4b17182000ddf4d158adc697c75a3942
oscar <oscar.vasquez@zentro.com.mx>
2024-04-12 16:02:42 -0600

    debug

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+server.listen(3000, '192.168.1.69' || 'localhost', function() {
+    console.log('Aplicación de NodeJS ' + port + " Iniciada...")
+})
