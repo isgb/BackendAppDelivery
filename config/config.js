ef923a34b7154ca0e9cf861ea04ec083ac24beec
oscar <oscar.vasquez@zentro.com.mx>
2024-04-17 16:31:36 -0600

    debug

--- config/config.js
+++ config/config.js
@@ -1,1 +1,15 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'udemy_delivery'
+});
+
+db.connect(function(err){
+    if(err) throw err;
+    console.log('DATABSE CONNECTED!');
+});
+
+module.exports = db;
