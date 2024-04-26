1c1146979b254018144f422afcc3f71aef824b3f
oscar <oscar.vasquez@zentro.com.mx>
2024-04-26 11:18:03 -0600

    debug

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,4 +3,5 @@ const userController = require('../controllers/usersController');
 module.exports = (app) => {
 
     app.post('/api/users/create', userController.register);
+    app.post('/api/users/create', userController.login);
 }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const logger = require('morgan')
 const cors = require('cors');
+const passport = require('passport')
 
 /**
  * IMPORTAR RUTAS
@@ -19,6 +20,10 @@ app.use(express.urlencoded({
 }));
 
 app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
 
 app.disable('x-powered-by')
 
