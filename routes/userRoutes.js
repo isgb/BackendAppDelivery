2499fc5869d92c0c9c72a3cc49d5b3850891e6b7
oscar <oscar.vasquez@zentro.com.mx>
2024-05-03 17:49:35 -0600

    debug

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 const userController = require('../controllers/usersController');
 
-module.exports = (app) => {
+module.exports = (app, upload) => {
 
     app.post('/api/users/create', userController.register);
+    app.post('/api/users/createWithImage', upload.array('image',1) ,userController.register);
     app.post('/api/users/login', userController.login);
 }
