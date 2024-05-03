950200c198c401d3105f178f2aec8a7046c7fd3b
oscar <oscar.vasquez@zentro.com.mx>
2024-05-03 17:32:04 -0600

    debug

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -80,6 +80,30 @@ module.exports = {
 
         });
 
+    },
+
+    registerWithImage(req, res) {
+
+        const user = req.body; // CAPTURO LOS DATOS QUE ME ENVIE EL CLIENTE
+        User.create(user, (err, data) => {
+
+            if (err) {
+                console.log('ENTRO EN EL ERROR');
+                return res.status(501).json({
+                    success: false,
+                    message: 'Hubo un error con el registro del usuario',
+                    error: err
+                });
+            }
+
+            return res.status(201).json({
+                success: true,
+                message: 'El registro se realizo correctamente',
+                data: data // EL ID DEL NUEVO USUARIO
+            });
+
+        });
+
     }
 
 }
