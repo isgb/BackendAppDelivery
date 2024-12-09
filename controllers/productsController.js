598d293c638274c0f9d7b141b111fd1a68933e4e
isaias <isgb_codigo@hotmail.com>
2024-12-08 18:35:27 -0600

    avances

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,166 +1,208 @@
-const Product = require("../models/product");
-const storage = require("../utils/cloud_storage");
-const asyncForEach = require("../utils/async_foreach");
+const Product = require('../models/product');
+const storage = require('../utils/cloud_storage');
+const asyncForEach = require('../utils/async_foreach');
+
 
 module.exports = {
-  async findByCategory(req, res) {
+    
+    findByCategory(req, res) {
+        const id_category = req.params.id_category;
 
-    const id_category = req.params.id_category;
-
-    Product.findByCategory(id_category, (err, data) => {
-      if (err) {
-        console.log("ENTRO EN EL ERROR");
-        return res.status(501).json({
-          success: false,
-          message: "Hubo un error al momento de listar las categorias",
-          error: err,
-        });
-      }
-
-      return res.status(201).json(data);
-    });
-  },
-
-  async create(req, res) {
-    const product = JSON.parse(req.body.product); // CAPTURO LOS DATOS QUE ME ENVIE EL CLIENTE
-
-    const files = req.files;
-    let inserts = 0;
-    if (files.length === 0) {
-      return res.status(501).json({
-        success: false,
-        message: "Error al registrar el producto no tiene imagenes.",
-      });
-    } else {
-      Product.create(product, (err, id_product) => {
-
-        if (err) {
-          console.log("ENTRO EN EL ERROR");
-          return res.status(501).json({
-            success: false,
-            message: "Hubo un error con el registro del producto",
-            error: err,
-          });
-        }
-
-        product.id = id_product;
-        const start = async () => {
-          await asyncForEach(files, async (file) => {
-            const path = `image_${Date.now()}`;
-            const url = await storage(file, path);
-
-            if (url != undefined && url != null) {
-
-                if(inserts === 0){
-                    product.image1 = url;
-                }
-                else if(inserts === 1){
-                    product.image2 = url;
-                }
-                else if(inserts === 2){
-                    product.image3 = url;
-                }
+        Product.findByCategory(id_category, (err, data) => {
+            if (err) {
+                return res.status(501).json({
+                    success: false,
+                    message: 'Hubo un error al momento de listar las categorias',
+                    error: err
+                });
             }
 
-            await Product.update(product, (err, data) => {
+            return res.status(201).json(data);
+        });
+    },
+
+    create(req, res) {
+
+        const product = JSON.parse(req.body.product); // CAPTURO LOS DATOS QUE ME ENVIE EL CLIENTE
+
+        const files = req.files;
+        
+        let inserts = 0; 
+        
+        if (files.length === 0) {
+            return res.status(501).json({
+                success: false,
+                message: 'Error al registrar el producto no tiene imagenes',
+            });
+        }
+        else {
+            Product.create(product, (err, id_product) => {
+
+        
                 if (err) {
                     return res.status(501).json({
-                      success: false,
-                      message: "Hubo un error con el registro del producto",
-                      error: err,
+                        success: false,
+                        message: 'Hubo un error con el registro del producto',
+                        error: err
                     });
-                  }
+                }
+                
+                product.id = id_product;
+                const start = async () => {
+                    await asyncForEach(files, async (file) => {
+                        const path = `image_${Date.now()}`;
+                        const url = await storage(file, path);
 
-                  inserts = inserts + 1;
+                        if (url != undefined && url != null) { // CREO LA IMAGEN EN FIREBASE
+                            if (inserts == 0) { //IMAGEN 1
+                                product.image1 = url;
+                            }
+                            else if (inserts == 1) { //IMAGEN 2
+                                product.image2 = url;
+                            }
+                            else if (inserts == 2) { //IMAGEN 3
+                                product.image3 = url;
+                            }
+                        }
 
-                  if(inserts === files.length){ // TERMINO DE ALMACENAR LAS TRES IMAGENES
-                    return res.status(201).json({
-                        success: true,
-                        message: "El producto se almaceno correctamente",
-                        data: data,
-                      }); 
-                  }
-            })
-          });
-        };
+                        await Product.update(product, (err, data) => {
+                            if (err) {
+                                return res.status(501).json({
+                                    success: false,
+                                    message: 'Hubo un error con el registro del producto',
+                                    error: err
+                                });
+                            }
 
-        start();
+                            inserts = inserts + 1;
 
-      });
-    }
-  },
+                            if (inserts == files.length) { // TERMINO DE ALAMACENAR LAS TRES IMAGENES
+                                return res.status(201).json({
+                                    success: true,
+                                    message: 'El producto se almaceno correctamente',
+                                    data: data
+                                });
+                            }
 
-  async updateWithImage(req, res) {
-    const category = JSON.parse(req.body.category); // CAPTURO LOS DATOS QUE ME ENVIE EL CLIENTE
+                        });
+                    });
+                }
+    
+                start();
+    
+            });
+        }
 
-    const files = req.files;
+    },
 
-    if (files.length > 0) {
-      const path = `image_${Date.now()}`;
-      const url = await storage(files[0], path);
+    update(req, res) {
+        const product = req.body;
 
-      if (url != undefined && url != null) {
-        category.image = url;
-      }
-    }
+        Product.update(product, (err, data) => {
+            if (err) {
+                return res.status(501).json({
+                    success: false,
+                    message: 'Hubo un error con el registro del producto',
+                    error: err
+                });
+            }
 
-    Category.update(category, (err, id) => {
-      if (err) {
-        console.log("ENTRO EN EL ERROR");
-        return res.status(501).json({
-          success: false,
-          message: "Hubo un error con la actualización del categoria",
-          error: err,
+            return res.status(201).json({
+                success: true,
+                message: 'El producto se actualizo correctamente',
+                data: data
+            });
+        })
+    },
+    updateWithImage(req, res) {
+        const product = JSON.parse(req.body.product); // CAPTURO LOS DATOS QUE ME ENVIE EL CLIENTE
+
+        const files = req.files;
+        
+        let inserts = 0; 
+        
+        if (files.length === 0) {
+            return res.status(501).json({
+                success: false,
+                message: 'Error al registrar el producto no tiene imagenes',
+            });
+        }
+        else {
+            Product.update(product, (err, id_product) => {
+
+                if (err) {
+                    return res.status(501).json({
+                        success: false,
+                        message: 'Hubo un error con la actualizacion del producto',
+                        error: err
+                    });
+                }
+                
+                product.id = id_product;
+                const start = async () => {
+                    await asyncForEach(files, async (file) => {
+                        const path = `image_${Date.now()}`;
+                        const url = await storage(file, path);
+
+                        if (url != undefined && url != null) { // CREO LA IMAGEN EN FIREBASE
+                            if (inserts == 0) { //IMAGEN 1
+                                product.image1 = url;
+                            }
+                            else if (inserts == 1) { //IMAGEN 2
+                                product.image2 = url;
+                            }
+                            else if (inserts == 2) { //IMAGEN 3
+                                product.image3 = url;
+                            }
+                        }
+
+                        await Product.update(product, (err, data) => {
+                            if (err) {
+                                return res.status(501).json({
+                                    success: false,
+                                    message: 'Hubo un error con la actualizacion del producto',
+                                    error: err
+                                });
+                            }
+
+                            inserts = inserts + 1;
+
+                            if (inserts == files.length) { // TERMINO DE ALAMACENAR LAS TRES IMAGENES
+                                return res.status(201).json({
+                                    success: true,
+                                    message: 'El producto se actualizo correctamente',
+                                    data: data
+                                });
+                            }
+
+                        });
+                    });
+                }
+    
+                start();
+    
+            });
+        }
+    },
+
+    delete(req, res) {
+        const id = req.params.id;
+
+        Product.delete(id, (err, id) => {
+            if (err) {
+                return res.status(501).json({
+                    success: false,
+                    message: 'Hubo un error al momento de eliminar el producto',
+                    error: err
+                });
+            }
+
+            return res.status(201).json({
+                success: true,
+                message: 'El producto se elimino correctamente',
+                data: `${id}`
+            });
         });
-      }
+    },
 
-      return res.status(201).json({
-        success: true,
-        message: "El categoria se actualizo correctamente",
-        data: `${id}`,
-      });
-    });
-  },
-
-  async update(req, res) {
-    const category = req.body; // CAPTURO LOS DATOS QUE ME ENVIE EL CLIENTE
-
-    Product.update(category, (err, id) => {
-      if (err) {
-        console.log("ENTRO EN EL ERROR");
-        return res.status(501).json({
-          success: false,
-          message: "Hubo un error con la actualización del categoria",
-          error: err,
-        });
-      }
-
-      return res.status(201).json({
-        success: true,
-        message: "El categoria se actualizo correctamente",
-        data: `${id}`,
-      });
-    });
-  },
-
-  async delete(req, res) {
-
-    const id = req.params.id;
-
-    Product.delete(id, (err, id) => {
-      if (err) {
-        console.log("ENTRO EN EL ERROR");
-        return res.status(501).json({
-          success: false,
-          message: "Hubo un error al momento de eliminar el producto",
-          error: err,
-        });
-      }
-      return res.status(201).json({
-        success: true,
-        message: "El producto se elimino correctamente",
-        data: `${id}`,
-      }); 
-    });
-  },
-};
+}
--- models/product.js
+++ models/product.js
@@ -1,90 +1,60 @@
 const db = require('../config/config');
+
 const Product = {};
-
-// Product.getAll = (result) => {
-//     const sql = `
-//     SELECT 
-//         id,
-//         name,
-//         description,
-//         image
-//     FROM
-//         categories
-//     ORDER BY
-//         name
-//     `;
-
-//     db.query(
-//         sql,
-//         (err, data) => {
-//             if(err){
-//                 console.log('Error:', err);
-//                 result(err, null);
-//             }
-//             else{
-//                 console.log('ID de la nueva categoria:', data);
-//                 result(null, data);
-//             }
-//         }
-//     )
-
-// }
-
 
 Product.findByCategory = (id_category, result) => {
     const sql = `
-        SELECT
-            P.id,
-            P.name,
-            P.description,
-            P.price,
-            P.image1,
-            P.image2,
-            P.image3,
-            P.id_category
-        FROM
-            products as P
-        WHERE
-            P.id_category = ?
+    SELECT
+        CONVERT(P.id, char) AS id,
+        P.name,
+        P.description,
+        P.price,
+        P.image1,
+        P.image2,
+        P.image3,
+        CONVERT(P.id_category, char) AS id_category
+    FROM
+        products as P
+    WHERE 
+        P.id_category = ?
     `;
 
     db.query(
         sql,
         [id_category],
         (err, res) => {
-            if(err){
+            if (err) {
                 console.log('Error:', err);
                 result(err, null);
             }
-            else{
-                console.log('ID del nuevo producto:', res);
+            else {
+                console.log('Id de la nuevo producto:', res);
                 result(null, res);
             }
         }
-    )
+    );
 }
 
 Product.create = (product, result) => {
 
-    const sql = ` 
-    INSERT INTO 
-        products 
-    (
-        name,
-        description,
-        price,
-        image1,
-        image2,
-        image3,
-        id_category,
-        created_at,
-        updated_at
-    )   
-    VALUES(?, ?, ?, ? ,?, ?, ?, ?, ?)
+    const sql = `
+    INSERT INTO
+        products(
+            name,
+            description,
+            price,
+            image1,
+            image2,
+            image3,
+            id_category,
+            created_at,
+            updated_at   
+        )
+    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
     `;
 
     db.query(
-        sql,
+        sql, 
         [
             product.name,
             product.description,
@@ -94,41 +64,44 @@ Product.create = (product, result) => {
             product.image3,
             product.id_category,
             new Date(),
-            new Date()
+            new Date(),
         ],
         (err, res) => {
-            if(err){
+            if (err) {
                 console.log('Error:', err);
                 result(err, null);
             }
-            else{
-                console.log('ID del nuevo producto:', res.insertId);
+            else {
+                console.log('Id de la nuevo producto:', res.insertId);
                 result(null, res.insertId);
             }
         }
+
     )
 
-};
+}
+
 
 Product.update = (product, result) => {
+
     const sql = `
-        UPDATE
-            products
-        SET
-            name = ?,
-            description = ?,
-            price = ?,
-            image1 = ?,
-            image2 = ?,
-            image3 = ?,
-            id_category = ?,
-            updated_at = ?
-        WHERE
-            id = ?
+    UPDATE
+        products
+    SET
+        name = ?,
+        description = ?,
+        price = ?,
+        image1 = ?,
+        image2 = ?,
+        image3 = ?,
+        id_category = ?,
+        updated_at = ?
+    WHERE
+        id = ?
     `;
 
     db.query(
-        sql,
+        sql, 
         [
             product.name,
             product.description,
@@ -140,14 +113,14 @@ Product.update = (product, result) => {
             new Date(),
             product.id
         ],
-        (err, data) => {
-            if(err){
+        (err, res) => {
+            if (err) {
                 console.log('Error:', err);
                 result(err, null);
             }
-            else{
-                console.log('ID del producto actualizado:', data.id);
-                result(null, data.id);
+            else {
+                console.log('Id del producto actualizado:', product.id);
+                result(null, product.id);
             }
         }
     )
@@ -155,51 +128,26 @@ Product.update = (product, result) => {
 
 Product.delete = (id, result) => {
     const sql = `
-        DELETE FROM
-            products
-        WHERE
-            id = ?
+    DELETE FROM
+        products
+    WHERE
+        id = ?
     `;
 
     db.query(
         sql,
         [id],
-        (err, data) => {
-            if(err){
+        (err, res) => {
+            if (err) {
                 console.log('Error:', err);
                 result(err, null);
             }
-            else{
-                console.log('ID del producto eliminado:', id);
+            else {
+                console.log('Id del producto eliminado:', id);
                 result(null, id);
             }
         }
     )
 }
-
-// Category.delete = (id,result) => {
-
-//     const sql = `
-//         DELETE FROM
-//             categories
-//         WHERE
-//             id = ?
-//     `;
-//     db.query(
-//         sql,
-//         id,
-//         (err, res) => {
-//             if(err){
-//                 console.log('Error:', err);
-//                 result(err, null);
-//             }
-//             else{
-//                 console.log('ID de la categoria eliminada:', id);
-//                 result(null, id);
-//             }
-//         }
-//     )
-
-// }
 
 module.exports = Product;
